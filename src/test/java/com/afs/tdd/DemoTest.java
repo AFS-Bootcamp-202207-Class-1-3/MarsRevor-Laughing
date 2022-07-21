d4285f127c51f7c15f28d1f0ed86a7bd85283156
Laughinggg <953651397@qq.com>
2022-07-21 15:21:20 +0800

    test: should_return_location01N_when_given_M

---
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -0,0 +1,14 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DemoTest {
+    @Test
+    void should_return_location01N_when_given_M() {
+        String excepted="location( x, y ), direction N";
+        String instruction="M";
+
+        assertEquals(excepted,instruction);
+    }
+}
